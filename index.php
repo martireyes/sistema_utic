e2e259de89ac0ebcd9fd50a9bf8e9b7a4e8a8dab
Martín Reyes <rreyes1700@gmail.com>
2026-09-11 15:53:28 -0300

    Actualización de login

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ if ($_SESSION){
         <meta name="theme-color" content="#007bff" media="(prefers-color-scheme: light)" />
         <meta name="theme-color" content="#1a1a1a" media="(prefers-color-scheme: dark)" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fontsource/source-sans-3@5.0.12/index.css" integrity="sha256-tXJfXfp6Ewt1ilPzLDtQnJV4hclT9XuaZUKyUvmyr+Q=" crossorigin="anonymous" media="print" onload="this.media = 'all'" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.11.0/styles/overlayscrollbars.min.css" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@4.9.1/dist/css/adminlte.min.css" />
@@ -21,39 +22,57 @@ if ($_SESSION){
     </head>
     <body>
         <div class="login-page bg-body-secondary">
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <img src="img/user.png" class="img-responsive" id="user"/>
+            <main class="login-box">
+                <h1 class="login-logo">
+                    <a href="index.php"><b>LP3</b> | Acceso</a>
+                </h1>
+                <div class="card">
+                    <div class="card-body login-card-body">
+                        <p class="login-box-msg">Ingrese sus credenciales para iniciar sesión</p>
+
+                        <form action="acceso.php" method="post">
+                            <label for="validationTooltipUsername" class="visually-hidden" for="loginUser">Usuario</label>
+                            <div class="input-group mb-3">
+                                <input id="loginUser" type="text" class="form-control" name="usuario" placeholder="Usuario" required>
+                                <div class="input-group-text">
+                                    <span class="bi bi-envelope"></span>
+                                </div>
+                            </div>
+                            <label class="visually-hidden" for="loginClave">Clave</label>
+                            <div class="input-group mb-3">
+                                <input id="loginClave" type="password" class="form-control" name="clave" placeholder="Clave"/>
+                                <div class="input-group-text">
+                                    <span class="bi bi-lock-fill"></span>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-8">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" name="recuerdame">
+                                        <label class="form-check-label" for="flexCheckDefault">Recuerdame</label>
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="d-grip gap-2">
+                                        <button type="submit" class="btn btn-primary">Ingresar</button>
+                                    </div>
+                                </div>
+                            </div>
+                           <!-- <?php //Mensaje de error
+                            if(!empty($_SESSION['error'])){?>
+                            <div class="alert alert-danger" role="alert">
+                                <span class="glyphicon glyphicon-exclamation-sign"></span>
+                                <?php echo $_SESSION['error']; ?>
+                                <div class="alert alert-danger" role="alert">
+                                    <span class="glyphicon glyphicon-exclamation-sign"></span>
+                                    Error de inicio
+                                </div>
+                            </div>
+                            <?php } ?> -->
+                        </form>
+                    </div>
                 </div>
-            </div>
-            <form class="login" action="acceso.php" method="post">
-                <div class="form-group has-feedback">
-                    <input type="text" class="form-control" name="usuario" required="" autofocus="" placeholder="Ingrese su usuario"/>
-                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                </div>
-                <div class="form-group has-feedback">
-                    <input type="password" class="form-control" name="clave" required="" autofocus="" placeholder="Ingrese su clave"/>
-                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                </div>
-                <button class="btn btn-lg btn-primary btn-block" type="submit">Iniciar Sesi&oacute;n</button>
-                <div class="checkbox">
-                    <label class="checkbox"><input type="checkbox" value="1" name="recuerdame"/>No cerrar sesión</label>
-                    <p class="help-block"><a href="#">¿No puede acceder a su cuenta?</a></p>
-                </div>
-                <?php
-                //Mensaje de error
-                if(!empty($_SESSION['error'])){?>
-                <div class="alert alert-danger" role="alert">
-                    <span class="glyphicon glyphicon-exclamation-sign"></span>
-                    <?php echo $_SESSION['error']; ?>
-                    <div class="alert alert-danger" role="alert">
-                        <span class="glyphicon glyphicon-exclamation-sign"></span>
-                        Error de inicio
-                    
-                 </div>
-                </div>
-                <?php } ?>
-            </form>
+            </main>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js"></script>
